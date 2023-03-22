e37c16b5d811600368d27c08ee23a0dca1005f5a
Jamil Lawrence <jamil.lawrence@gmail.com>
2023-03-21 20:47:32 -0500

    Advanced login session complete

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,6 +11,10 @@ class UserTest < ActiveSupport::TestCase
       )
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?("")
+  end
+
   test "should be valid" do
     assert @user.valid?
   end
