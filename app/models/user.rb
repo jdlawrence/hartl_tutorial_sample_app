c0069b36a48e940641f071414715da318225a773
Jamil Lawrence <jamil.lawrence@gmail.com>
2023-04-13 21:21:15 -0500

    working on microfeed

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,7 +87,10 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest: User.digest(self.reset_token), reset_sent_at: Time.zone.now)
+    update_columns(
+      reset_digest: User.digest(self.reset_token),
+      reset_sent_at: Time.zone.now
+    )
     # update_attribute(:reset_digest, User.digest(reset_token))
     # update_attribute(:reset_sent_at, Time.zone.now)
   end
@@ -100,6 +103,12 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
