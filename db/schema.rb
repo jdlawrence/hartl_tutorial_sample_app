2cf4a2d885245fcf961048acab87506cb58bcb61
Jamil Lawrence <jamil.lawrence@gmail.com>
2023-08-01 11:06:34 -0500

    Fixed image resizing in production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_185156) do
+ActiveRecord::Schema.define(version: 2023_07_31_181404) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
